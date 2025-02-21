1ffde0204b51ba86eae02f09115f4114e037ab92
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-21 19:18:14 +0900

    feature: 메일 에러 추가

--- src/main/java/kr/co/iabacus/sales/core/common/error/ErrorCode.java
+++ src/main/java/kr/co/iabacus/sales/core/common/error/ErrorCode.java
@@ -17,13 +17,17 @@ public enum ErrorCode {
     // MEMBER
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "member.not.found"),
 
-    // PASSWORD
+    // AUTH
     PASSWORD_SAME(HttpStatus.UNAUTHORIZED, "password.same"),
     PASSWORD_LENGTH(HttpStatus.UNAUTHORIZED, "password.length"),
     PASSWORD_UPPERCASE(HttpStatus.UNAUTHORIZED, "password.uppercase"),
     PASSWORD_LOWERCASE(HttpStatus.UNAUTHORIZED, "password.lowercase"),
     PASSWORD_SPECIAL_CHARACTER(HttpStatus.UNAUTHORIZED, "password.special.character"),
-    PASSWORD_CONFIRM(HttpStatus.UNAUTHORIZED, "password.confirm");
+    PASSWORD_CONFIRM(HttpStatus.UNAUTHORIZED, "password.confirm"),
+
+    // MAIL
+    MAIL_SEND_FAIL(HttpStatus.BAD_REQUEST, "mail.send.fail"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
