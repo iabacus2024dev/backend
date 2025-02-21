03012b6a3497fb899c2c199acc3fe760bbd30544
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-21 20:21:05 +0900

    test: AuthService 테스트 - 회원 등록, 비밀번호 초기화 테스트

---
+++ src/test/java/kr/co/iabacus/sales/web/auth/service/AuthServiceTest.java
@@ -0,0 +1,98 @@
+package kr.co.iabacus.sales.web.auth.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.co.iabacus.sales.web.auth.domain.Auth;
+import kr.co.iabacus.sales.web.auth.dto.MemberRegisterRequest;
+import kr.co.iabacus.sales.web.auth.dto.PasswordInitializeRequest;
+import kr.co.iabacus.sales.web.auth.repository.AuthRepository;
+import kr.co.iabacus.sales.web.mail.MailService;
+import kr.co.iabacus.sales.web.member.domain.Member;
+import kr.co.iabacus.sales.web.member.repository.MemberRepository;
+
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @MockitoBean
+    private MailService mailService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private AuthRepository authRepository;
+
+    @Test
+    @DisplayName("이름과 이메일을 입력받아 회원을 등록하면 auth 테이블에 이메일과 토큰이 저장되고 메일이 전송된다.")
+    void registerMember() {
+        // given
+        String name = "박상철";
+        String email = "example@exmple.com";
+        memberRepository.save(Member.builder()
+            .name(name)
+            .email(email)
+            .build());
+
+        MemberRegisterRequest request = MemberRegisterRequest.builder()
+            .name(name)
+            .email(email)
+            .build();
+
+        // stubbing
+        doNothing().when(mailService).sendInitializePasswordLink(anyString(), anyString());
+
+        // when
+        authService.registerMember(request);
+        Auth auths = authRepository.findByEmail(email).orElseThrow();
+
+        // then
+        assertThat(auths.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    @DisplayName("메일로 전송된 토큰으로 비밀번호 초기화를 하면 비밀번호가 초기화된다.")
+    void initializePassword() {
+        // given
+        String name = "박상철";
+        String email = "example@exmple.com";
+        memberRepository.save(Member.builder()
+            .name(name)
+            .email(email)
+            .build());
+
+        authService.registerMember(MemberRegisterRequest.builder()
+            .name(name)
+            .email(email)
+            .build());
+
+        PasswordInitializeRequest request = PasswordInitializeRequest.builder()
+            .token(authRepository.findByEmail(email).orElseThrow().getToken())
+            .newPassword("Password1234!")
+            .newPasswordConfirm("Password1234!")
+            .build();
+
+        // when
+        authService.initializePassword(request, LocalDateTime.of(2025, 1, 1, 0, 0, 0));
+        Member member = memberRepository.findByEmailAndIsActivatedTrue(email).orElseThrow();
+
+        // then
+        assertThat(member.getPassword()).isNotNull();
+    }
+
+}
