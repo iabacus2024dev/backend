219bf02bb08af7fdabd176d88e096c51b7e5f4c4
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-24 21:42:33 +0900

    feature: 메일 전송 비동기 처리

--- src/main/java/kr/co/iabacus/sales/web/mail/MailClient.java
+++ src/main/java/kr/co/iabacus/sales/web/mail/MailClient.java
@@ -1,11 +1,12 @@
 package kr.co.iabacus.sales.web.mail;
 
-import jakarta.mail.Message;
+import jakarta.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,6 @@ import kr.co.iabacus.sales.core.common.error.exception.BusinessException;
 
 @Slf4j
 @RequiredArgsConstructor
-@Transactional
 @Component
 public class MailClient {
 
@@ -24,15 +24,20 @@ public class MailClient {
     @Value("${spring.mail.username}")
     private String from;
 
+    @Async
     public void send(String to, String subject, String message) {
         log.info("Send mail from: {}, to: {}, subject: {}, message: {}", from, to, subject, message);
         try {
-            javaMailSender.send(mimeMessage -> {
-                mimeMessage.setFrom(from);
-                mimeMessage.setRecipients(Message.RecipientType.TO, to);
-                mimeMessage.setSubject(subject);
-                mimeMessage.setText(message);
-            });
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(message, true); // HTML 지원
+
+            javaMailSender.send(mimeMessage);
+            log.info("Mail sent successfully to {}", to);
         } catch (Exception e) {
             log.error("Send mail error: {}", e.getMessage());
             throw new BusinessException(ErrorCode.MAIL_SEND_FAIL);
