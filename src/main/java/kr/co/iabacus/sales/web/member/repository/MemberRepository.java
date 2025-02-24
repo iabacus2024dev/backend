1350f94fdb5b8c0475baf85b7aacb0a139e66171
jisoo6622 <dlwltn6604@iabacus.co.kr>
2025-02-24 23:04:47 +0900

    feature: 구성원 상세 조회를 위해 추가

--- src/main/java/kr/co/iabacus/sales/web/member/repository/MemberRepository.java
+++ src/main/java/kr/co/iabacus/sales/web/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package kr.co.iabacus.sales.web.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kr.co.iabacus.sales.web.member.domain.Member;
 
@@ -13,5 +15,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndNameAndIsActivatedTrue(String email, String name);
 
     Optional<Member> findByEmailAndIsActivatedTrue(String email);
+
+    @Query("SELECT m FROM Member m " +
+        "LEFT JOIN FETCH m.rank " +
+        "LEFT JOIN FETCH m.grade " +
+        "LEFT JOIN FETCH m.type " +
+        "WHERE m.id = :memberId AND m.isLocked = false")
+    Optional<Member> findMemberDetailById(@Param("memberId") Long memberId);
 
 }
