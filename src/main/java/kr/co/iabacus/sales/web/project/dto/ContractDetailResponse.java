cd6446f6f54da6d3fab4ca677036f80d466db521
Hajin-Abacus <qwertyv@iabacus.co.kr>
2025-02-25 10:07:45 +0900

    feature: 계약 상세 dto 추가

---
+++ src/main/java/kr/co/iabacus/sales/web/project/dto/ContractDetailResponse.java
@@ -0,0 +1,33 @@
+package kr.co.iabacus.sales.web.project.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import kr.co.iabacus.sales.web.project.domain.Contract;
+import kr.co.iabacus.sales.web.project.domain.ContractStatus;
+
+@Getter
+@Builder
+public class ContractDetailResponse {
+
+    private UUID id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private ContractStatus status;
+    private LocalDate actualStartDate;
+    private LocalDate actualEndDate;
+
+    public static ContractDetailResponse from(Contract contract) {
+        return ContractDetailResponse.builder()
+            .id(contract.getId())
+            .startDate(contract.getStartDate())
+            .endDate(contract.getEndDate())
+            .actualStartDate(contract.getActualStartDate())
+            .actualEndDate(contract.getActualEndDate())
+            .build();
+    }
+
+}
