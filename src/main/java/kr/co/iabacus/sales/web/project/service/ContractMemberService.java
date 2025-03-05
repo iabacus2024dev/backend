c6c28f820e456c9e36b5a5a2714a7a74c50b1203
YUUY <55330m@iabacus.co.kr>
2025-03-05 11:43:29 +0900

    투입인원 상세조회 API

--- src/main/java/kr/co/iabacus/sales/web/project/service/ContractMemberService.java
+++ src/main/java/kr/co/iabacus/sales/web/project/service/ContractMemberService.java
@@ -16,8 +16,10 @@ import kr.co.iabacus.sales.web.member.domain.Member;
 import kr.co.iabacus.sales.web.member.repository.MemberRepository;
 import kr.co.iabacus.sales.web.partners.domain.Partners;
 import kr.co.iabacus.sales.web.project.domain.ContractMember;
+import kr.co.iabacus.sales.web.project.dto.ContractMemberDetailResponse;
 import kr.co.iabacus.sales.web.project.dto.ContractMemberResponse;
 import kr.co.iabacus.sales.web.project.repository.ContractMemberRepository;
+
 
 
 @Slf4j
@@ -27,6 +29,17 @@ public class ContractMemberService {
 
     private final ContractMemberRepository contractMemberRepository;
     private final MemberRepository memberRepository;
+
+
+    @Transactional(readOnly = true)
+    public ContractMemberDetailResponse getContractMemberDetail(UUID id){
+        ContractMember contractMember = contractMemberRepository.findById(id)
+            .orElseThrow(() -> new BusinessException(ErrorCode.CONTRACTMEMBER_NOT_FOUND));
+        Member member = memberRepository.findByIdAndIsActivatedTrue(contractMember.getMemberId())
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+
+        return ContractMemberDetailResponse.from(contractMember, member);
+    }
 
     @Transactional(readOnly = true)
     public List<ContractMemberResponse> getContractMembers(UUID contractId){
