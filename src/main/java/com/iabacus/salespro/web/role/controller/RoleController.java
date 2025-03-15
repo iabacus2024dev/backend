cc9c0511641f502ca1fdfafbc22baceb449ac8c9
sangcheol <tkdcjf38@iabacus.co.kr>
2025-03-16 01:57:04 +0900

    feature: 로그인한 이용자의 권한 조회 API 및 phone 버그 수정

---
+++ src/main/java/com/iabacus/salespro/web/role/controller/RoleController.java
@@ -0,0 +1,31 @@
+package com.iabacus.salespro.web.role.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iabacus.salespro.core.security.service.UserPrincipal;
+import com.iabacus.salespro.web.role.response.AuthorityResponse;
+import com.iabacus.salespro.web.role.service.RoleService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping
+    public ResponseEntity<List<AuthorityResponse>> getRoleWithAuthorities(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return ResponseEntity.ok(roleService.getRoleWithAuthorities(userPrincipal.getMemberId()));
+    }
+
+}
---
+++ src/main/java/com/iabacus/salespro/web/role/service/RoleService.java
@@ -0,0 +1,24 @@
+package com.iabacus.salespro.web.role.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iabacus.salespro.web.role.repository.RoleRepository;
+import com.iabacus.salespro.web.role.response.AuthorityResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public List<AuthorityResponse> getRoleWithAuthorities(Long memberId) {
+        return roleRepository.findByMemberIdWithAuthority(memberId).stream().map(AuthorityResponse::from).toList();
+    }
+
+}
