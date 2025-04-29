65fa509786ad8441ee87a018b22c119e370760ac
sangcheol <scpark0698@gmail.com>
2025-04-29 21:04:19 +0900

    fix: base url 설정

--- src/main/java/com/iabacus/salespro/core/security/config/SecurityConfig.java
+++ src/main/java/com/iabacus/salespro/core/security/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package com.iabacus.salespro.core.security.config;
 
-import java.util.Collections;
-import java.util.List;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iabacus.salespro.core.security.filter.CsrfCookieFilter;
+import com.iabacus.salespro.core.security.filter.CustomAuthFilter;
+import com.iabacus.salespro.core.security.filter.CustomAuthorizationFilter;
+import com.iabacus.salespro.core.security.handler.*;
+import com.iabacus.salespro.core.security.provider.CustomUserDetailsAuthenticationProvider;
+import com.iabacus.salespro.web.login.repository.LoginHistoryRepository;
+import com.iabacus.salespro.web.member.repository.MemberRepository;
+import com.iabacus.salespro.web.role.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,22 +32,8 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 import org.springframework.web.cors.CorsConfiguration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.RequiredArgsConstructor;
-
-import com.iabacus.salespro.core.security.filter.CsrfCookieFilter;
-import com.iabacus.salespro.core.security.filter.CustomAuthFilter;
-import com.iabacus.salespro.core.security.filter.CustomAuthorizationFilter;
-import com.iabacus.salespro.core.security.handler.CustomAccessDeniedHandler;
-import com.iabacus.salespro.core.security.handler.CustomBasicAuthenticationEntryPoint;
-import com.iabacus.salespro.core.security.handler.CustomLoginFailHandler;
-import com.iabacus.salespro.core.security.handler.CustomLoginSuccessHandler;
-import com.iabacus.salespro.core.security.handler.CustomLogoutSuccessHandler;
-import com.iabacus.salespro.core.security.provider.CustomUserDetailsAuthenticationProvider;
-import com.iabacus.salespro.web.login.repository.LoginHistoryRepository;
-import com.iabacus.salespro.web.member.repository.MemberRepository;
-import com.iabacus.salespro.web.role.repository.RoleRepository;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @EnableMethodSecurity
@@ -68,40 +61,40 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(corsConfig -> corsConfig.configurationSource(request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList(baseUrl));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setExposedHeaders(List.of("Authorization"));
-                config.setAllowCredentials(true);
-                config.setMaxAge(CORS_MAX_AGE);
-                return config;
-            }))
+                .cors(corsConfig -> corsConfig.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of(baseUrl, "http://localhost:5173"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(List.of("Authorization"));
+                    config.setAllowCredentials(true);
+                    config.setMaxAge(CORS_MAX_AGE);
+                    return config;
+                }))
 
-            .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
-                .ignoringRequestMatchers("/api/v1/auths/**", "/swagger-ui/**", "/api-docs/**")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .addFilterAfter(new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(usernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new CustomAuthorizationFilter(roleRepository), UsernamePasswordAuthenticationFilter.class)
-            .addFilterAfter(new RememberMeAuthenticationFilter(authenticationManager(), rememberMeServices()), UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+                        .ignoringRequestMatchers("/api/v1/auths/**", "/swagger-ui/**", "/api-docs/**")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(usernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new CustomAuthorizationFilter(roleRepository), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(new RememberMeAuthenticationFilter(authenticationManager(), rememberMeServices()), UsernamePasswordAuthenticationFilter.class)
 
-            .logout(config -> config
-                .logoutUrl(LOGOUT_URI)
-                .logoutSuccessHandler(new CustomLogoutSuccessHandler())
-                .deleteCookies("SESSION", "remember-me")
-            )
+                .logout(config -> config
+                        .logoutUrl(LOGOUT_URI)
+                        .logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                        .deleteCookies("SESSION", "remember-me")
+                )
 
-            .exceptionHandling(e -> {
-                e.accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper));
-                e.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint(objectMapper));
-            })
+                .exceptionHandling(e -> {
+                    e.accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper));
+                    e.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint(objectMapper));
+                })
 
-            .authorizeHttpRequests(requests -> requests
-                .requestMatchers("/api/v1/auths/**", "/swagger-ui/**", "/api-docs/**").permitAll()
-                .anyRequest().authenticated()
-            );
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/v1/auths/**", "/swagger-ui/**", "/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         setRequiresChannel(http);
         return http.build();
@@ -144,10 +137,10 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager() {
         ProviderManager providerManager = new ProviderManager(new CustomUserDetailsAuthenticationProvider(
-            userDetailsService,
-            passwordEncoder(),
-            memberRepository,
-            loginHistoryRepository
+                userDetailsService,
+                passwordEncoder(),
+                memberRepository,
+                loginHistoryRepository
         ));
         providerManager.setEraseCredentialsAfterAuthentication(false);
         return providerManager;
